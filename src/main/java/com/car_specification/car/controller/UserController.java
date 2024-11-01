b86ff78857cdf7657bf182273a02c1bc131fd8a6
ShivaKrishna2266 <gadudulashiva@gmail.com>
2024-11-02 01:11:08 +0530

    Resolved Corse Error

--- src/main/java/com/car_specification/car/controller/UserController.java
+++ src/main/java/com/car_specification/car/controller/UserController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/user")
 public class UserController {
